d6747529af3bbebdc8cc39b1440a8b01560d4c68
ThaoDoan2002 <doanthithao20022003@gmail.com>
2023-07-20 15:57:37 +0700

    Thuc hanh chuong 5

---
+++ MoveObject.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace LTGDC5
+{
+    partial class MoveObject
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelPic = new FlowLayoutPanel();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // panelPic
+            // 
+            panelPic.AutoScroll = true;
+            panelPic.BackColor = Color.FromArgb(255, 192, 192);
+            panelPic.FlowDirection = FlowDirection.TopDown;
+            panelPic.Location = new Point(664, 12);
+            panelPic.Name = "panelPic";
+            panelPic.Size = new Size(124, 424);
+            panelPic.TabIndex = 0;
+            panelPic.MouseDown += panelPic_MouseDown;
+            panelPic.MouseMove += panelPic_MouseMove;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 15F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.Blue;
+            label1.Location = new Point(3, 408);
+            label1.Name = "label1";
+            label1.Size = new Size(655, 28);
+            label1.TabIndex = 1;
+            label1.Text = "Dung chuot hoac cac phim mui ten di chuyen hinh vao panel ben phai";
+            // 
+            // MoveObject
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(panelPic);
+            Name = "MoveObject";
+            Text = "MoveObject";
+            Load += MoveObject_Load;
+            KeyDown += MoveObject_KeyDown;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private FlowLayoutPanel panelPic;
+        private Label label1;
+    }
+}
---
+++ MoveObject.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LTGDC5
+{
+    public partial class MoveObject : Form
+    {
+        Random rand = new Random();
+        string[] fileNames;
+        Point p;
+        int count;
+        public MoveObject()
+        {
+            InitializeComponent();
+        }
+
+        private void MoveObject_Load(object sender, EventArgs e)
+        {
+            fileNames = Directory.GetFiles(Application.StartupPath + @"\Image");
+            this.addPictureBox();
+        }
+
+        private void addPictureBox()
+        {
+            count++;//Ho tro viec tim kiem control theo Name
+            PictureBox pic = new PictureBox();
+            pic = new PictureBox();
+            pic.Name = count.ToString();
+            pic.Image = Image.FromFile(fileNames[rand.Next(fileNames.Length)]);
+            pic.SizeMode = PictureBoxSizeMode.StretchImage;
+            pic.Location = new Point(0, 0);
+            pic.Size = new Size(90, 90);
+            this.Controls.Add(pic);
+            pic.MouseDown += Pic_MouseDown;
+            pic.MouseMove += Pic_MouseMove;
+            pic.MouseUp += Pic_MouseUp;
+
+            this.Controls.SetChildIndex(this.panelPic, this.Controls.Count - 1);
+        }
+
+
+
+
+        private void Pic_MouseUp(object? sender, MouseEventArgs e)
+        {
+            PictureBox pic = (PictureBox)sender;
+            if (panelPic.Bounds.Contains(pic.Bounds))
+            {
+                panelPic.Controls.Add(pic);
+                this.addPictureBox();
+            }
+        }
+
+        private void Pic_MouseMove(object? sender, MouseEventArgs e)
+        {
+            PictureBox pic = (PictureBox)sender;
+            if (e.Button == MouseButtons.Left)
+            {
+                pic.Left += e.Location.X - p.X;
+                pic.Top += e.Location.Y - p.Y;
+            }
+        }
+
+        private void Pic_MouseDown(object? sender, MouseEventArgs e)
+        {
+            p = e.Location;
+        }
+
+        private void MoveObject_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Tim kiem theo Name cua control, false nghia la khong tim kiem bao
+            //gom con ma control do chua.
+            Control[] control = this.Controls.Find(count.ToString(), false);
+            PictureBox picture = (PictureBox)control[0]; // Match dung control theo Name dau tien.
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    if (picture.Top > 0)
+                        picture.Top -= 10;
+                    break;
+                case Keys.Down:
+                    if (picture.Bottom < ClientRectangle.Height)
+                        picture.Top += 10;
+                    break;
+                case Keys.Left:
+                    if (picture.Left > 0)
+                        picture.Left -= 10;
+                    break;
+                case Keys.Right:
+                    if (picture.Right < ClientRectangle.Width)
+                        picture.Left += 10;
+                    break;
+            }
+            if (panelPic.Bounds.Contains(picture.Bounds))
+            {
+                panelPic.Controls.Add(picture);
+                this.addPictureBox();
+            }
+        }
+
+        private void panelPic_MouseDown(object sender, MouseEventArgs e)
+        {
+            p = e.Location;
+        }
+
+        private void panelPic_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                panelPic.Left += e.Location.X - p.X;
+                panelPic.Top += e.Location.Y - p.Y;
+            }
+        }
+
+    }
+}
